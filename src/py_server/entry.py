c83ab71b6d45d5bcf50c38973dad098989519c83
Boris <b.vasilica@1step.md>
2026-05-24 23:14:21 +0300

    update to 18.1.2 version

--- src/py_server/entry.py
+++ src/py_server/entry.py
@@ -1,6 +1,5 @@
 import argparse
 import asyncio
-from pathlib import Path
 from py_server.main import main
 
 parser = argparse.ArgumentParser()
@@ -8,4 +7,9 @@ parser.add_argument("mode", nargs="?", default="stdio", choices=["stdio", "http"
 parser.add_argument("--env-file", type=str, default=None)
 args = parser.parse_args()
 
-asyncio.run(main(env_file=args.env_file, mode=args.mode))
+try:
+    asyncio.run(main(env_file=args.env_file, mode=args.mode))
+except KeyboardInterrupt:
+    pass
+except asyncio.CancelledError:
+    pass
